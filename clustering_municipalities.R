ca28e0cd463071bc46b04e1b9a61b4848e99bd9a
lostconnectionhere <rozsabir1@gmail.com>
2023-09-13 16:32:57 +0200

    initial clustering municipalities, in 5 clusters

--- clustering_municipalities.R
+++ clustering_municipalities.R
@@ -2,31 +2,29 @@ library(tidyverse)
 library(tidyr)
 library(dplyr)
 
-#Inlezen dataset
+#Inlezen dataset + analyseren
 kerncijfers <- read.csv2('data/regionale_kerncijfers_subset.csv', sep=";")
 print(kerncijfers)
 
 colnames(kerncijfers) 
 
-# kerncijfers[, c('Perioden')]
 
-#Clusters op basis van data uit het jaar 2022
+# Data uit alleen het jaar 2022 gebruiken voor de clusters
 kerncijfers_2022 <- kerncijfers[kerncijfers$Perioden %in% '2022JJ00',]
 
-#Controleren of in de kolom Perioden alleen 2022 staat
-# dim(kerncijfers_2022)
+# Controleren of in de kolom Perioden alleen 2022 staat
 kerncijfers_2022[, c('Perioden')]
 
 #Onderzoek of er lege rijen zijn
 print("Totaal aantal missende waarden - ")
 sum(is.na(kerncijfers_2022))
 
-colSums(is.na(kerncijfers_2022))
 kerncijfers_2022 <- na.omit(kerncijfers_2022)
 colSums(is.na(kerncijfers_2022))
 
-# Categorische waarde eruit om te clusteren, zoals ID
+# Categorische kolommen verwijderen, ID, RegioS & Perioden
 kerncijfers_2022 <- kerncijfers_2022[, !names(kerncijfers_2022) %in% c("ID")]
+kerncijfers_2022 <- kerncijfers_2022[, !names(kerncijfers_2022) %in% c("Perioden")]
 
 # RegioS kolom verplaatsen naar rijnummer
 row.names(kerncijfers_2022) <- kerncijfers_2022$RegioS
@@ -34,45 +32,32 @@ row.names(kerncijfers_2022) <- kerncijfers_2022$RegioS
 # RegioS kolom verwijdered
 kerncijfers_2022 <- kerncijfers_2022[, !names(kerncijfers_2022) %in% c("RegioS")]
 
-# Check of goed is gegaan
+# Controleren of dataset alleen uit numerieke waarden bestaat
 summary(kerncijfers_2022)
 colnames(kerncijfers_2022)
 
+# voor reproduceerbaarheid seed 123 gekozen
 set.seed(123)
 
-kerncijfers_2022 <- as.numeric(unlist(x))
-
-
-
-# sum(sapply(kerncijfers_2022, is.na))
-# sum(sapply(kerncijfers_2022, is.infinite))
-# sum(sapply(kerncijfers_2022, is.nan))
-# 
-# kerncijfers_2022[apply(sapply(kerncijfers_2022, is.finite), 1, all),]
-
-# sum(sapply(kerncijfers_2022, is.na))
-# sum(!sapply(kerncijfers_2022, is.finite))
 
 km.out <- kmeans(kerncijfers_2022, centers = 5, nstart = 20)
 km.out
 
-# Decide how many clusters to look at
+# Max clusters op 10
 n_clusters <- 10
 
-# Initialize total within sum of squares error: wss
+# Sum of squares initialiseren
 wss <- numeric(n_clusters)
 
-set.seed(123)
-
-# Look over 1 to n possible clusters
+# De verschillende aantallen clusters analyseren
 for (i in 1:n) {
-  # Fit the model: km.out
+  # fit het model: km.out
   km.out <- kmeans(kerncijfers_2022, centers = i, nstart = 20)
-  # Save the within cluster sum of squares
+  # within cluster sum of squares
   wss[i] <- km.out$tot.withinss
 }
 
-# Produce a scree plot
+# scree plot
 wss_df <- tibble(clusters = 1:n, wss = wss)
 
 scree_plot <- ggplot(wss_df, aes(x = clusters, y = wss, group = 1)) +
@@ -82,4 +67,14 @@ scree_plot <- ggplot(wss_df, aes(x = clusters, y = wss, group = 1)) +
   xlab('Number of clusters')
 
 scree_plot
+
+scree_plot +
+  geom_hline(
+    yintercept = wss, 
+    linetype = 'dashed', 
+    col = c(rep('#000000',4),'#FF0000', rep('#000000', 5))
+  )
+
+# Plot de 5 clusters
+fviz_cluster(km.out, kerncijfers_2022, ellipse.type = "norm")
 
--- practice/clustering_irisdataset.R
+++ practice/clustering_irisdataset.R
@@ -1,11 +1,19 @@
 data("iris")
 head(iris)
-
+iris.scaled <- scale(iris[, -5])
 x=iris[,3:4]
 head(x)
 
-model=kmeans(x,3)
-  library(cluster)
-clusplot(x,model$cluster)
+# model=kmeans(x,3)
+#   library(cluster)
+# clusplot(x,model$cluster)
+# 
 
+# K-means clustering
+km.res <- kmeans(iris.scaled, 3, nstart = 10)
 
+# Visualize kmeans clustering
+fviz_cluster(km.res, iris[, -5], ellipse.type = "norm")
+
+fviz_cluster(km.res, iris[, -5],
+             palette = "Set2", ggtheme = theme_minimal())
