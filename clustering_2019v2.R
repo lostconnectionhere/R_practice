8c22c401922b61954cb17f978289d8c0b63d2ca4
lostconnectionhere <rozsabir1@gmail.com>
2023-10-25 16:09:41 +0200

    analysis dataset done

--- clustering_2019v2.R
+++ clustering_2019v2.R
@@ -10,3 +10,53 @@ df1 <- read.csv2('data/FVW_2019.csv', sep=";")
 
 # Data analyseren
 head(df1) 
+
+# Kolomnamen wijzigen voor leesbaarheid
+colnames(df1)[colnames(df1) == "X"] <- "Gemeente"
+colnames(df1)[colnames(df1) == "Inwoners.naar.geslacht.en.leeftijd.Totaal.mannen.en.vrouwen.Totaal.mannen.en.vrouwen"] <- "Inwoners.Totaal"
+print(df1$Gemeente)
+
+# Verwijder de laatste 4 rijen, die staan voor irrelevante data bijv. Buitenland
+df1 <- head(df1, n = nrow(df1) - 4)
+print(df1$Gemeente)
+
+# Verwijder alle gemeenten die NA hebben voor aantal inwoners
+na_rows <- is.na(df1$Inwoners.Totaal)
+print(df1[na_rows,])
+df1 <- df1[!na_rows, ]
+
+# Functie om leading en trailing spaties te verwijderen voor character- of factor-kolommen
+trim_character_or_factor <- function(df) {
+  df[] <- lapply(df, function(col) {
+    if (is.character(col) || is.factor(col)) {
+      return(trimws(as.character(col)))
+    } else {
+      return(col)
+    }
+  })
+  return(df)
+}
+
+# Pas de functie toe op je dataframes df1, df2, en df3
+df1 <- trim_character_or_factor(df1)
+
+# Hoeveel punten bij iedere kolom aanwezig
+apply(df1, 2,  function(col) sum(col == '.'))
+
+# Alle punten omzetten in NA's
+df1 <- df1 %>% mutate(across(.cols = everything(), ~replace(., . == '.', NA)))
+
+# Hoeveel missende waarden per kolom
+sapply(df1, function(x) sum(is.na(x)))
+
+# Welke class hebben alle kolommen
+sapply(df1, class)
+
+# Gemeentenaam kolom verplaatsen naar rijnummer
+row.names(df1) <- df1$Gemeente
+
+# Gemeentenaam kolom verwijdered
+new_df <- df1[, !names(df1) %in% c("Gemeente")]
+names(new_df)
+
+
